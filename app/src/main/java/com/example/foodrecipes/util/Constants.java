d0a5d105e0596f5435a874bcb2a8b05d7d120da1
Paul <paulcobzaru0@gmail.com>
2019-08-28 15:26:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/foodrecipes/util/Constants.java
@@ -0,0 +1,7 @@
+package com.example.foodrecipes.util;
+
+public class Constants {
+    public static String BASE_URL = "https://www.food2fork.com";
+
+    public static String API_KEY = "e59b2bc53972f98a95d4f2cbc0e2adee";
+}
