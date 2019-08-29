98b8676be9cd3ddaa531a554ba6a54359c132f0b
Paul <paulcobzaru0@gmail.com>
2019-08-29 11:27:20 +0300

    retrofit

--- app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
@@ -3,9 +3,27 @@ package com.example.foodrecipes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.example.foodrecipes.models.Recipe;
+import com.example.foodrecipes.requests.RecipeApi;
+import com.example.foodrecipes.requests.ServiceGenerator;
+import com.example.foodrecipes.requests.responses.RecipeResponse;
+import com.example.foodrecipes.requests.responses.RecipeSearchResponse;
+import com.example.foodrecipes.util.Constants;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class RecipeListActivity extends BaseActivity {
+
+    private static final String TAG = "RecipeListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +33,7 @@ public class RecipeListActivity extends BaseActivity {
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mProgressBar.getVisibility() == View.VISIBLE) {
-                    showProgressBar(false);
-                } else {
-                    showProgressBar(true);
-                }
+
             }
         });
     }
