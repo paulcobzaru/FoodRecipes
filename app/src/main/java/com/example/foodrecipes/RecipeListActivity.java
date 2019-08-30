023222c298b2e5c6f21902e0d143b8cf119be2a8
Paul <paulcobzaru0@gmail.com>
2019-08-30 22:32:42 +0300

    loading animation

--- app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
@@ -1,40 +1,40 @@
 package com.example.foodrecipes;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodrecipes.adapters.OnRecipeListener;
+import com.example.foodrecipes.adapters.RecipeRecyclerAdapter;
 import com.example.foodrecipes.models.Recipe;
 import com.example.foodrecipes.util.Testing;
 import com.example.foodrecipes.viewmodels.RecipeListViewModel;
 
 import java.util.List;
 
-public class RecipeListActivity extends BaseActivity {
+public class RecipeListActivity extends BaseActivity implements OnRecipeListener {
 
     private static final String TAG = "RecipeListActivity";
 
     private RecipeListViewModel mRecipeListViewModel;
+    private RecyclerView mRecyclerView;
+    private RecipeRecyclerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
 
+        mRecyclerView = findViewById(R.id.recipe_list);
         mRecipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel.class);
 
+        initRecyclerView();
         subscribeObservers();
-
-        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                searchRecipesApi("chicken breast", 1);
-            }
-        });
+        initSearchView();
     }
 
     private void subscribeObservers() {
@@ -43,12 +43,42 @@ public class RecipeListActivity extends BaseActivity {
             public void onChanged(List<Recipe> recipes) {
                 if(recipes != null) {
                     Testing.printRecipes(recipes, "recipes test");
+                    mAdapter.setRecipes(recipes);
                 }
             }
         });
     }
 
-    public void searchRecipesApi(String query, int pageNumber) {
-        mRecipeListViewModel.searchRecipesApi(query, pageNumber);
+    private void initRecyclerView() {
+        mAdapter = new RecipeRecyclerAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void initSearchView() {
+        final SearchView searchView = findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                mAdapter.displayLoading();
+                mRecipeListViewModel.searchRecipesApi(s, 1);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onRecipeClick(int position) {
+
+    }
+
+    @Override
+    public void onCategoryClick(String category) {
+
     }
 }
